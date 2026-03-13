4bfdcd22a37895eb8adf00f1535a4fc00cb6ac84
arianarmw <arianarahmawati0910@gmail.com>
2026-03-13 20:50:59 +0700

    Add condition for menu tambah lagu and cari lagu

--- PlaylistOOP.java
+++ PlaylistOOP.java
@@ -7,7 +7,8 @@
 2902734941 - Ardelia Cindy Wulandari
 */
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 class Lagu{
     private String judul;
@@ -24,30 +25,18 @@ class Lagu{
         return judul;
     }
 
-    public void setJudul(String judul){
-        this.judul = judul;
-    }
-
     public String getArtis(){
         return artis;
-    }
-
-    public void setArtis(String artis){
-        this.artis = artis;
     }
 
     public double getDurasi(){
         return durasi;
     }
 
-    public void setDurasi(double durasi){
-        this.durasi = durasi;
-    }
-
-    void tamplikanInfo(){
+    void tampilkanInfo(){
         System.out.println("Judul: " + judul);
         System.out.println("Artis: " + artis);
-        System.out.println("Durasi: " + durasi + "\n");
+        System.out.println("Durasi: " + durasi + " menit\n");
     }
 }
 
@@ -64,6 +53,7 @@ class User {
 }
 
 class Admin extends User{
+
     public Admin(String name){
         super(name);
     }
@@ -73,26 +63,53 @@ class Admin extends User{
         System.out.println("Admin: menambahkan lagu baru ke playlist");
     }
 
-    public int tambahLagu(Lagu[] playlist, int jumlahLagu, Lagu laguBaru){
-        if(jumlahLagu < playlist.length){
-            playlist[jumlahLagu] = laguBaru;
-            jumlahLagu++;
-            System.out.println("Lagu berhasil ditambahkan");
-        } else {
-            System.out.println("Playlist sudah penuh!");
-        }
-        return jumlahLagu;
-    }
+    public void tambahLagu(Lagu laguBaru){
 
-    public void lihatLagu(Lagu[] playlist, int jumlahLagu){
-        System.out.println("\n=== Daftar Lagu ===");
-        for(int i = 0; i < jumlahLagu; i++){
-            playlist[i].tamplikanInfo();
+        try{
+
+            File file = new File("playlist.txt");
+
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            Scanner cekFile = new Scanner(file);
+
+            while(cekFile.hasNextLine()){
+                String line = cekFile.nextLine();
+                String[] data = line.split(";");
+
+                String judul = data[0];
+                String artis = data[1];
+
+                if(judul.equalsIgnoreCase(laguBaru.getJudul()) &&
+                   artis.equalsIgnoreCase(laguBaru.getArtis())){
+
+                    System.out.println("Lagu dari artis yang sama sudah terdaftar!");
+                    cekFile.close();
+                    return;
+                }
+            }
+
+            cekFile.close();
+
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(laguBaru.getJudul() + ";" +
+                         laguBaru.getArtis() + ";" +
+                         laguBaru.getDurasi() + "\n");
+
+            writer.close();
+
+            System.out.println("Lagu berhasil ditambahkan!");
+
+        }catch(IOException e){
+            System.out.println("Terjadi kesalahan saat menyimpan file.");
         }
     }
 }
 
 class Member extends User{
+
     public Member(String name){
         super(name);
     }
@@ -102,48 +119,133 @@ class Member extends User{
         System.out.println("Member: melihat dan mencari lagu");
     }
 
-    public void lihatLagu(Lagu[] playlist, int jumlahLagu){
-        System.out.println("\n=== Daftar Lagu ===");
-        for(int i = 0; i < jumlahLagu; i++){
-            playlist[i].tamplikanInfo();
-        }
-    }
+    public void lihatLagu(){
 
-    public void cariLagu(Lagu[] playlist, int jumlahLagu, String cariJudul){
-        for(int i = 0; i < jumlahLagu; i++){
-            if(playlist[i].getJudul().equalsIgnoreCase(cariJudul)){
-                System.out.println("\nLagu Ditemukan: ");
-                playlist[i].tamplikanInfo();
+        try{
+
+            File file = new File("playlist.txt");
+
+            if(!file.exists()){
+                System.out.println("Belum ada lagu di playlist.");
+                return;
             }
-        }
 
-        if(playlist == null){
-            System.out.println("Lagu tidak ditemukan!");
+            Scanner reader = new Scanner(file);
+
+            System.out.println("\n=== Daftar Lagu ===");
+
+            while(reader.hasNextLine()){
+
+                String line = reader.nextLine();
+                String[] data = line.split(";");
+
+                Lagu lagu = new Lagu(
+                        data[0],
+                        data[1],
+                        Double.parseDouble(data[2])
+                );
+
+                lagu.tampilkanInfo();
+            }
+
+            reader.close();
+
+        }catch(Exception e){
+            System.out.println("Terjadi kesalahan saat membaca file.");
         }
     }
 
-    public void rataRataDurasiLagu(Lagu[] playlist, int jumlahLagu){
+    public void cariLagu(String cariJudul){
+
+        boolean ditemukan = false;
+
+        try{
+
+            File file = new File("playlist.txt");
+
+            if(!file.exists()){
+                System.out.println("Playlist masih kosong.");
+                return;
+            }
+
+            Scanner reader = new Scanner(file);
+
+            while(reader.hasNextLine()){
+
+                String line = reader.nextLine();
+                String[] data = line.split(";");
+
+                if(data[0].equalsIgnoreCase(cariJudul)){
+
+                    Lagu lagu = new Lagu(
+                            data[0],
+                            data[1],
+                            Double.parseDouble(data[2])
+                    );
+
+                    System.out.println("\nLagu ditemukan:");
+                    lagu.tampilkanInfo();
+
+                    ditemukan = true;
+                }
+            }
+
+            reader.close();
+
+            if(!ditemukan){
+                System.out.println("Lagu belum tersedia.");
+            }
+
+        }catch(Exception e){
+            System.out.println("Terjadi kesalahan saat mencari lagu.");
+        }
+    }
+
+    public void rataRataDurasiLagu(){
+
         double total = 0;
+        int jumlah = 0;
 
-        for(int i = 0; i < jumlahLagu; i++){
-            total += playlist[i].getDurasi();
-        }
+        try{
 
-        if(jumlahLagu > 0){
-            double rataRata = total / jumlahLagu;
-            System.out.println("Rata-rata durasi lagu: " + rataRata + " menit");
-        } else {
-            System.out.println("Belum ada lagu");
+            File file = new File("playlist.txt");
+
+            if(!file.exists()){
+                System.out.println("Belum ada lagu.");
+                return;
+            }
+
+            Scanner reader = new Scanner(file);
+
+            while(reader.hasNextLine()){
+
+                String line = reader.nextLine();
+                String[] data = line.split(";");
+
+                total += Double.parseDouble(data[2]);
+                jumlah++;
+            }
+
+            reader.close();
+
+            if(jumlah > 0){
+                double rata = total / jumlah;
+                System.out.printf("Rata-rata durasi lagu: %.2f menit\n", rata);
+            }else{
+                System.out.println("Belum ada lagu.");
+            }
+
+        }catch(Exception e){
+            System.out.println("Terjadi kesalahan.");
         }
-    } 
+    }
 }
 
 public class PlaylistOOP {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        Lagu[] playlist = new Lagu[10];
-        int jumlahLagu = 0;
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
 
         Admin admin = new Admin("Admin");
         Member member = new Member("Member");
@@ -151,19 +253,22 @@ public class PlaylistOOP {
         int pilihan;
 
         do{
-            System.out.println("\n === MENU ===");
+
+            System.out.println("\n=== MENU ===");
             System.out.println("1. Tambah Lagu");
             System.out.println("2. Lihat Lagu");
             System.out.println("3. Cari Lagu");
             System.out.println("4. Rata-Rata Durasi Lagu");
             System.out.println("5. Keluar");
 
-            System.out.print("Masukan Pilihan: ");
+            System.out.print("Masukkan Pilihan: ");
             pilihan = input.nextInt();
-
             input.nextLine();
-            switch (pilihan) {
+
+            switch(pilihan){
+
                 case 1:
+
                     System.out.print("Judul Lagu: ");
                     String judul = input.nextLine();
 
@@ -174,31 +279,34 @@ public class PlaylistOOP {
                     double durasi = input.nextDouble();
 
                     Lagu laguBaru = new Lagu(judul, artis, durasi);
+                    admin.tambahLagu(laguBaru);
 
-                    jumlahLagu = admin.tambahLagu(playlist, jumlahLagu, laguBaru);
                     break;
-                
+
                 case 2:
-                    member.lihatLagu(playlist, jumlahLagu);
+                    member.lihatLagu();
                     break;
-                
+
                 case 3:
                     System.out.print("Cari Judul Lagu: ");
                     String cariJudul = input.nextLine();
-                    member.cariLagu(playlist, jumlahLagu, cariJudul);
+                    member.cariLagu(cariJudul);
                     break;
-                
+
                 case 4:
-                    member.rataRataDurasiLagu(playlist, jumlahLagu);
+                    member.rataRataDurasiLagu();
                     break;
-                
+
                 case 5:
-                    System.out.println("Program Selesai");
+                    System.out.println("Program selesai.");
                     break;
+
                 default:
-                    System.out.println("Pilihan tidak valid!");;
+                    System.out.println("Pilihan tidak valid!");
             }
-        } while (pilihan != 5);
+
+        }while(pilihan != 5);
+
         input.close();
     }
 }
